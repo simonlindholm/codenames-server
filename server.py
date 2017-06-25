61f363f4b7126b19c2b10f2fa48048e64a8830fb
Simon Lindholm <simon.lindholm10@gmail.com>
2017-06-25 17:26:17 +0200

    Port server to flask

--- server.py
+++ server.py
@@ -1,64 +1,66 @@
-import http.server
-import threading
-import socketserver
-import urllib
-import json
-import os
+from flask import *
+# from flask_compress import Compress
 from subprocess import Popen, PIPE
+import json
 
-class JSONError(Exception):
-    pass
+app = Flask(__name__)
 
-class Handler(http.server.SimpleHTTPRequestHandler):
+class ApiError(Exception):
 
-    def handleClueAPI(self, params):
-        try:
-            engine = params['engine'][0]
-            color = params['color'][0]
-            colors = params['colors'][0]
-            words = params['words'][0].replace(' ', '_').split(',')
-            index = int(params['index'][0])
-            count = int(params['count'][0])
-            if len(colors) != len(words):
-                raise JSONError("colors and words have different lengths")
-            if color not in "rb" or any(col not in "rbca" for col in colors):
-                raise JSONError("invalid colors")
-            inp = engine + ' ' + color + '\n'
-            inp += '\n'.join(c + ' ' + w for (c, w) in zip(colors, words)) + '\n'
-            inp += 'go ' + str(index) + ' ' + str(count) + '\n'
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code or 200
+        self.payload = payload
 
-            proc = Popen(['./codenames', '--batch'], stdin=PIPE, stdout=PIPE, cwd='./Codenames')
-            outp = proc.communicate(inp.encode())[0]
-            self.wfile.write(outp)
-        except KeyError as e:
-            raise JSONError("missing parameter: {}".format(e.args[0]))
-        except ValueError:
-            raise JSONError("cannot parse numeric parameter")
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['status'] = 0
+        rv['message'] = self.message
+        return rv
 
-    def do_GET(self):
-        path = urllib.parse.urlparse(self.path)
-        if path.path == '/api/1/clue':
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.send_header("Access-Control-Allow-Origin", "*")
-            self.end_headers()
-            try:
-                self.handleClueAPI(urllib.parse.parse_qs(path.query))
-            except JSONError as e:
-                self.wfile.write(json.dumps({"status": 0, "message": e.args[0]}).encode())
-        else:
-            return super().do_GET()
+@app.errorhandler(ApiError)
+def handle_api_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
-    def translate_path(self, path):
-        ret = super().translate_path(path)
-        start = os.getcwd()
-        assert ret.startswith(start)
-        return start + '/static' + ret[len(start):]
+@app.route("/api/1/clue")
+def clueAPI():
+    def arg(id, default=None, type=None):
+        if not id in request.args:
+            raise ApiError("Missing parameter " + str(id))
+        return request.args.get(id, default=default, type=type)
+    engine = arg('engine')
+    color = arg('color')
+    colors = arg('colors')
+    words = arg('words').replace(' ', '_').split(',')
+    index = arg('index', -1, int)
+    count = arg('count', -1, int)
+    if len(colors) != len(words):
+        raise ApiError("colors and words have different lengths")
+    if color not in "rb" or any(col not in "rbca" for col in colors):
+        raise ApiError("invalid colors")
 
-class ThreadedHTTPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
+    inp = engine + ' ' + color + '\n'
+    inp += '\n'.join(c + ' ' + w for (c, w) in zip(colors, words)) + '\n'
+    inp += 'go ' + str(index) + ' ' + str(count) + '\n'
 
-print('Server listening on port 10000...')
-ThreadedHTTPServer.allow_reuse_address = True
-httpd = ThreadedHTTPServer(('', 10000), Handler)
-httpd.serve_forever()
+    proc = Popen(['./codenames', '--batch'], stdin=PIPE, stdout=PIPE, cwd='./Codenames')
+    outp = proc.communicate(inp.encode())[0]
+
+    resp = Response(outp)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Content-type'] = 'application/json'
+    return resp
+
+@app.route("/<path:path>")
+def statics(path):
+    return send_from_directory('static', path)
+
+@app.route("/")
+def main():
+    return send_file('static/index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
